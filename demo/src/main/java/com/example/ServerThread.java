37cda1c960a26bf9006de5e7b8ae66b3ed112a49
lai2-24 <falli.matteo@itismeucci.com>
2023-11-21 14:37:01 +0100

    fifth commit

--- demo/src/main/java/com/example/ServerThread.java
+++ demo/src/main/java/com/example/ServerThread.java
@@ -2,15 +2,25 @@ package com.example;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ServerThread extends Thread{
     protected Socket socket;
     protected String username;
+    protected BufferedReader in;
+    protected DataOutputStream out;
 
     public ServerThread(Socket socket) {
         this.socket = socket;
+        this.username = "";
+        try{
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new DataOutputStream(socket.getOutputStream());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public String getUsername(){
@@ -21,80 +31,76 @@ public class ServerThread extends Thread{
         return this.socket;
     }
 
+    public boolean setUsername(String string) throws IOException{
+        boolean kek = true;
+        for(int i=0; i<App.personeinchat.size(); i++){
+            if(App.personeinchat.get(i).getUsername().equals(username)){
+                out.writeBytes("!\n");
+                kek = false;
+            }
+        }
+        if(kek){
+            this.username = string;
+            out.writeBytes(".\n");
+        }
+        return kek;
+    }
+
+    public void sendBroadcast(String string) throws IOException{
+        out.writeBytes(".\n");
+        for(int i=0; i<App.personeinchat.size(); i++){
+            if(!this.username.equals(App.personeinchat.get(i).getUsername())){
+                DataOutputStream outsingle = new DataOutputStream(App.personeinchat.get(i).getSocket().getOutputStream());
+                outsingle.writeBytes(string + "\n");
+            }
+        }
+    }
+
+    public void sendMessage(String string) throws IOException{
+        boolean kek = false;
+        DataOutputStream outsingle = null;
+        for(int i=0; i<App.personeinchat.size(); i++){
+            if(App.personeinchat.get(i).getUsername().equals(username)){
+                outsingle = new DataOutputStream(App.personeinchat.get(i).getSocket().getOutputStream());
+                kek=true;
+            }
+        }
+        if(kek){
+            outsingle.writeBytes(string + "\n");
+        }else{
+            out.writeBytes("Utente non trovato\n");
+        }
+    }
+
     @Override
     public void run(){
-
         try {
-            
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            String username;
-            String message;
+            String incoming;
+            String message[];
             boolean exit = false;
-            boolean kek;
-            do{
-                kek = true;
-                username = in.readLine();
-                for(int i=0; i<App.personeinchat.size(); i++){
-                    if(App.personeinchat.get(i).getUsername().equals(username)){
-                        out.writeBytes("!\n");
-                        kek = false;
-                    }
-                }
-                if(kek){
-                    out.writeBytes(".\n");
-                }
-            }while(!kek);
-            for(int i=0; i<App.personeinchat.size(); i++){
-                if(!this.username.equals(App.personeinchat.get(i).getUsername())){
-                    DataOutputStream outsingle=new DataOutputStream(App.personeinchat.get(i).getSocket().getOutputStream());
-                    outsingle.writeBytes("+\n");
-                    outsingle.writeBytes(this.username + "\n");
-                }
-            }
+            setUsername("");
+            System.out.println("L'utente " + this.username + " si è connesso");
+            sendBroadcast("L'utente " + this.username + " si è connesso alla chat");
             while(!exit){
-                username = in.readLine();
-                switch(username){
+                incoming = in.readLine();
+                message = incoming.split(":");
+                switch(message[0]){
                     case "@everyone":
-                        out.writeBytes(".\n");
-                        message = in.readLine();
-                        for(int i=0; i<App.personeinchat.size(); i++){
-                            if(!this.username.equals(App.personeinchat.get(i).getUsername())){
-                                DataOutputStream outsingle=new DataOutputStream(App.personeinchat.get(i).getSocket().getOutputStream());
-                                outsingle.writeBytes("@\n");
-                                outsingle.writeBytes(this.username + "\n");
-                                outsingle.writeBytes(message + "\n");
-                            }
-                        }
+                        System.out.println("L'utente " + this.username + " vuole comunicare in broadcast");
+                        sendBroadcast(this.username + " scrive: " + message[1]);
                         break;
                     case "@exit":
-                        out.writeBytes("&\n");
+                        System.out.println("L'utente " + this.username + " vuole chiudere la connessione");
                         exit = true;
-                        for(int i=0; i<App.personeinchat.size(); i++){
-                            if(!this.username.equals(App.personeinchat.get(i).getUsername())){
-                                DataOutputStream outsingle=new DataOutputStream(App.personeinchat.get(i).getSocket().getOutputStream());
-                                outsingle.writeBytes("-\n");
-                                outsingle.writeBytes(this.username + "\n");
-                            }
-                        }
+                        sendBroadcast("L'utente " + this.username + " ha abbandonato la chat");
+                        break;
+                    case "@username":
+                        System.out.println("L'utente " + this.username + " vuole cambiare il suo username");
+                        setUsername("");
                         break;
                     default:
-                        DataOutputStream outsingle = null;
-                        for(int i=0; i<App.personeinchat.size(); i++){
-                            if(App.personeinchat.get(i).getUsername().equals(username)){
-                                outsingle = new DataOutputStream(App.personeinchat.get(i).getSocket().getOutputStream());
-                                kek=true;
-                            }
-                        }
-                        if(kek){
-                            out.writeBytes(".\n");
-                            message = in.readLine();
-                            outsingle.writeBytes("#\n");
-                            outsingle.writeBytes(this.username + "\n");
-                            outsingle.writeBytes(message + "\n");
-                        }else{
-                            out.writeBytes("!\n");
-                        }
+                        System.out.println("L'utente " + this.username + " vuole comunicare in privato");
+                        sendMessage(message[1]);
                 }
             }
         } catch (Exception e) {
